dffbcbdbe6836121f453743a41a3d4cd896b86d1
Paul Worthington <pworthington17@gmail.com>
2024-06-15 10:01:07 +0000

    program 1 will loop if required

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+#random module to select random number from list length
 import random
+#date time module to calculate time deltas between searches
 from datetime import datetime
 
 def create_ordered_list(list_length_request):
@@ -33,7 +36,7 @@ def welcome_user():
         print('Loading Program...')
         return 'program'
     else:
-        while learn_or_play != 'y' or 'n':
+        while learn_or_play != 'y' or 'n': #loops if user inputs unknown entry into console
             print('Please enter the character y and press enter if you would like to learn more about binary search or the character n if you would like to run the program')
             next_input = input()
             if next_input == 'y':
@@ -47,7 +50,12 @@ def welcome_user():
 
 def program_type():
     """
-    Function to ask the user what program type they would like to play
+    Function to ask the user what program type they would like to play.
+    Program 1 is using search methods to iterate through a user specified list length for a random number
+    and provides the time delta between the two methods.
+
+    Program 2 takes a user specified list lengh and iterates through it normally and using binary search 
+    for each of the items within the list. It provides the user with x and y data to plot the result on a graph
     """
     print('Enter the number 1 to use Search Methods on a single user selected list length. Enter the number 2 to use Search Methods to generate x and y data or type instructions if you would like to read the instructions.')
     program_type = input()
@@ -58,7 +66,7 @@ def program_type():
     elif program_type == 'instructions':
         return 'instructions'
     else:
-         while program_type != '1' or '2' or 'instructions':
+         while program_type != '1' or '2' or 'instructions': #loops if user inputs unknown entry to console
             print('Please enter the number 1 to use Search Methods on a single user selected list length. Enter the number 2 to use Search Methods to generate x and y data or type instructions if you would like to read the instructions.')
             next_input = input()
             if program_type == '1':
@@ -75,20 +83,36 @@ def instructions():
     if they want to learn further
     """
     create_separation()
-    print('Instructions')
+    print('Instructions') #to be completed
     create_separation()
-    print('Would you like to run this program y/n')
-    run_program = input()
-    if run_program == 'y':
-        main()
-    else:
-        print('Closing program')
+    print('If you would like use Search Methods on a single user selected list length enter 1.')
+    print('If you would like to use use search methods to generate x and y data enter the number 2.') 
+    print('Finally, if you would like to end this program type end into the console')
+    user_selection = input()
+    if user_selection == '1':
+        program_one()
+    elif user_selection == '2':
+        program_two()
+    elif user_selection == 'end':
         exit()
+    else:
+        while user_selection != '1' or '2' or 'instructions' or 'end': #loops if user provides unknown input
+            print('It seems like you have not entered a correct input. Please enter the number 1 to If you would like use Search Methods on a single user selected list length enter 1, if you would like to use use search methods to generate x and y data enter the number 2. Finally, if you would like to end this program type end into the console')
+            second_user_input = input()
+            if second_user_input == '1':
+                program_one()
+            elif second_user_input == '2':
+                program_two()
+            elif second_user_input == 'end':
+                exit()
 
 def get_list_length():
     """
     Function used to obtain the users desired list length between 100 and 10000000
     will convert to int and assess if within the specified values
+
+    10 million selected as a reasonable list length for program to run in a short time frame
+    but also provide reaonable output
     """
     print('Please input a list length integer between 100 (one hundred) and 10000000 (10 million). Any floats will be rounded to the nearest whole number')
     list_request = input()
@@ -100,7 +124,7 @@ def get_list_length():
         create_separation()
         return list_request_num
     else:
-        while list_request_num < 100 or list_request_num > 10000000:
+        while list_request_num < 100 or list_request_num > 10000000: #prevents user from inputing list length greater than or shorter than intended
             print('Please enter a number between 100 (one hundred) and 10000000 (10 million)')
             second_list_request = input()
             second_list_request_num = int(round(float(second_list_request)))
@@ -121,6 +145,10 @@ def create_separation():
         i += 1
 
 def select_random(x):
+    """
+    Function used to select a random list index number within the user specified list length. 
+    Uses random module imported from above
+    """
     start = x[0]
     end = x[-1]
     random_list_item = random.randint(start, end)
@@ -143,6 +171,9 @@ def normal_iteration(created_list, random_selection):
     return abs(time_delta)
 
 def binary_search_loop(list, low, high, random_item):
+    """
+    Function to loop through the list and identify the searched for item
+    """
  
     if high >= low:
  
@@ -158,6 +189,9 @@ def binary_search_loop(list, low, high, random_item):
         return -1
  
 def binary_search(list, random_item):
+    """
+    Function to set up binary_search_loops (above) and provide it with the 4 statments required
+    """
     
     print('Performing binary search \n')
     start = datetime.now().microsecond
@@ -169,20 +203,65 @@ def binary_search(list, random_item):
     return abs(time_delta)
 
 def compare_time_delta (normal, binary):
+    """
+    Function to calculate the time difference between normal iteration and binary search 
+    """
     time_taken = normal - binary
     return time_taken
 
 def time_delta_realisation(time, list_length, random_item):
+    """
+    Function used to take the time delta data and provide the user with feedback. 
+    This function also coverts the microseconds into seconds and minutes to further demostrate the differences 
+    in search methods
+    """
     print(f'The time difference between binary search and normal iteration is {time} when searching a list made up of {list_length} items and a randomly selected search item of {random_item}')
-    create_separation
+    create_separation()
     time_over_hnths = time * 100000
     time_seconds = time_over_hnths * 0.0000001
     time_min = time_seconds / 60
-    create_separation
-    print(f'Over 100000 iterations through the same list this would equate to {round(time_seconds, 2)} seconds or {round(time_min, 2)} minutes. rounded to two decimal places')
-    create_separation
+    print(f'Over 100000 iterations through the same list this would equate to {round(time_seconds, 2)} seconds or {round(time_min, 2)} minutes. Rounded to two decimal places')
+    create_separation()
+
+def program_complete():
+    """
+    Followingf the completion of program 1 and program 2 this function provides the user with options on
+    running program1 or 2 again, reading the instructions or ending the program
+    """
+    print('Thank you for using search methods. I hope you found the result interesting and informative.')
+    create_separation()
+    print('If you would like use Search Methods on a single user selected list length enter 1.')
+    print('If you would like to use use search methods to generate x and y data enter the number 2.')
+    print('If you would like to read the again type instructions into the console.') 
+    print('Finally, if you would like to end this program type end into the console')
+    user_selection = input()
+    if user_selection == '1':
+        program_one()
+    elif user_selection == '2':
+        program_two()
+    elif user_selection == 'instructions':
+        instructions()
+    elif user_selection == 'end':
+        exit()
+    else:
+        while user_selection != '1' or '2' or 'instructions' or 'end':
+            print('It seems like you have not entered a correct input. Please enter the number 1 to If you would like use Search Methods on a single user selected list length enter 1, if you would like to use use search methods to generate x and y data enter the number 2. If you would like to read the instructions type instructions into the console. Finally, if you would like to end this program type end into the console')
+            second_user_input = input()
+            if second_user_input == '1':
+                program_one()
+            elif second_user_input == '2':
+                program_two()
+            elif second_user_input == 'instructions':
+                instructions()
+            elif second_user_input == 'end':
+                exit()
+
     
 def program_one():
+    """
+    This function runs the operation of program 1 containing all of the functions required to provide the user with the
+    intended output
+    """
     list_length = get_list_length()
     list = create_ordered_list(list_length)
     random_item = select_random(list)
@@ -190,8 +269,13 @@ def program_one():
     time_delta_binary = binary_search(list, random_item)
     time_taken = compare_time_delta(time_delta_normal, time_delta_binary)
     result = time_delta_realisation(time_taken, list_length, random_item)
+    program_complete()
 
 def program_two():
+    """
+    This function runs the operation of program 2 containing all of the functions required to provide the user with the
+    intended output
+    """
     print('program two is not available yet')
     main()
 
